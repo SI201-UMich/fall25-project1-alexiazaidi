dc22db3237389a91593138c9195d7347a9c41ba2
Alexia Zaidi <alexiazaidi@Alexias-MacBook-Pro-2.local>
2025-10-06 16:24:17 -0400

    finished def load_data function

--- main.py
+++ main.py
@@ -2,5 +2,19 @@ class data_set_manager():
     def __init__(self, file_name):
         self.file_name = file_name
         self.data_dict = {}
+    
+    def load_data(self):
+        with open(self.file_name, 'r') as file:
+            headers = []
+            my_list = []
+            counter = 0
+            for line in file:
+                if counter == 0:
+                    headers.append(line)
+                    counter +=1
+                else:
+                    my_list.append(line)
+            self.headers = headers 
+            self.data_list = my_list
 
     
